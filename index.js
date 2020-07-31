8acdb266258f5636336b3c498e78fad019ff18f6
unknown <aser1989@sina.com>
2020-07-31 17:09:37 +0800

    add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import station from './src/station'
 import Convert from './src/until'
 import {Timer, SingleTimer, CountDown} from './src/timer'
 import {mixin as _mixin} from "./src/mixin";
+import {exportExcel as _exportExcel} from './src/exportExcel'
 
 const StationManager = station;
 const TimerModel = {
@@ -9,10 +10,12 @@ const TimerModel = {
 }
 
 export const mixin = _mixin;
+export const exportExcel = _exportExcel;
 export {
     StationManager,
     Convert,
     Timer,       //计时管理，依赖计时模型
     SingleTimer, //单例计时管理
-    TimerModel   //计时模型
+    TimerModel,  //计时模型
+    exportExcel
 }
